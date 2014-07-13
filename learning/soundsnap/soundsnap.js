034b3e6b7b4ee474888c7deeee6d3d4daccd7da6
Ben Borowski <ben.borowski@typeoneerror.com>
2014-07-13 09:20:47 -0700

    Added some examples

---
+++ learning/soundsnap/soundsnap.js
@@ -0,0 +1,28 @@
+var tessel  = require('tessel')
+  , ambient_lib = require('ambient-attx4')
+  , ambient = ambient_lib.use(tessel.port['A']);
+
+var sound_threshold     = 0.02
+  , reading_speed       = 250
+  , sound_trigger_delay = 1000;
+
+ambient.on('ready', function() {
+  setInterval(function() {
+    ambient.getSoundLevel(function(err, sdata) {
+      console.log('Sound level:', sdata.toFixed(8));
+    });
+  }, reading_speed);
+
+  ambient.setSoundTrigger(sound_threshold);
+
+  ambient.on('sound-trigger', function(data) {
+    console.log('Sound triggered', data);
+
+    ambient.clearSoundTrigger();
+
+    setTimeout(function() {
+      ambient.setSoundTrigger(sound_threshold);
+    }, sound_trigger_delay);
+
+  });
+});
